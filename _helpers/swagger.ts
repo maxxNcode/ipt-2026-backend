3759cca64aab52e9e604c4a83dc18be644b66fd6
maxxNcode <markfroilan2nd@gmail.com>
2026-05-21 16:08:43 +0800

    Fix Vercel ENOENT error by converting swagger.yaml to swagger.json

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./swagger.yaml');
+import swaggerDocument from '../swagger.json';
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
